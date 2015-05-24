ba302349366f95123bad90b72858257d2a323a2d
Raghav Kandubai <theguyraghav@gmail.com>
2015-05-24 03:23:39 -0400

    missed a file

--- src/src.ino
+++ src/src.ino
@@ -4,17 +4,17 @@
 
 // =============Arduino============
 #import <Arduino.h>
+#include <Wire.h>
+// ============Memory============
+#include "Driver_Memory.h"
 
 // =============BLE============
 #include <SoftwareSerial.h>
 #include "Driver_BLE.h"
 
-// ============Memory============
-#include "Driver_Memory.h"
+// =============Accelerometer============
+#include "MPU6050_Lib.h"
 
-// ============Accelerometer============
-
-#include "Wire.h"
 // ============Motor============
 #include "Driver_Motor.h"
 
@@ -33,7 +33,6 @@ int statusLED = true;
 //				   FUNCTIONS DECLARATIONS				 	//
 //**********************************************************************************************//
 
-void readBLE();
 
 
 //**********************************************************************************************//
@@ -44,11 +43,14 @@ void readBLE();
 //The arduino runs the setup function first
 void setup() 
 {
+//     Serial.println("start of loop1");
   pinMode(13,OUTPUT);
   Serial.begin(9600);
   BLE_init();
-  Motor_init();
+  motor_init();
+  //   Serial.println("start of loop2");
   accel_init();
+    // Serial.println("start of loop3");
 }
 
 
@@ -56,15 +58,17 @@ void setup()
 //the arduino keeps running the loop function until we shut it off
 void loop() 
 {
-  // delay(50); digitalWrite(13,statusLED); statusLED = !statusLED;
+ //  Serial.println("start of loop4");
+   delay(50); digitalWrite(13,statusLED); statusLED = !statusLED;
   
   // read commands sent by user
   readBLE();
-  
+   //  Serial.println("start of loop5");
   // execute the user commands
   executeCommandFromUser();
-  
-  //Serial.println(getAngle());
+
+  // Serial.println("start of loop6");
+  Serial.println(getAngle());
   
 }
 
@@ -72,24 +76,4 @@ void loop()
 //					HELPER FUNCTIONS  				 	//
 //**********************************************************************************************//
 
-
-void readBLE() {
-
-  while (BLE.available()) {
-    // get the new byte:
-    char inChar = (char)BLE.read(); 
-    //Serial.write(inChar);
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a semi colon, set a flag
-    // so the main loop can do something about it:
-    if (inChar == ';') {
-//      int pos = inputString.indexOf(';');
-//      inputString[pos+1] = '\n';
-      stringComplete = true;
-      Serial.println(inputString);
-    } 
-  }
-  
-}
 
